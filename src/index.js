c37ff6be93dd535f68fc2261f4fa1ee52ea15b5a
wasswa-derick <derick.wasswa@andela.com>
2018-07-26 09:50:37 +0300

    [Feature #157000832] Create store to connect the application actions to the reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { applicationReducers } from "./reducers/combine-reducers";
+import JwtDecode from "jwt-decode";
+import { loginUser } from "./actions/authentication";
+
+const store = createStore(applicationReducers);
+
+const appAccessToken = localStorage.getItem("appAccessToken");
+if (appAccessToken) {
+  const user = JwtDecode(appAccessToken);
+  store.dispatch(loginUser({ ...user, logInStatus: true }));
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
