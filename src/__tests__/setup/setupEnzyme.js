5db0feebbec85e0d19f5116d29f70ec5fbc199d0
wasswa-derick <derick.wasswa@andela.com>
2018-08-31 10:14:18 +0300

    Set up environment for testing

---
+++ src/__tests__/setup/setupEnzyme.js
@@ -0,0 +1,10 @@
+var enzyme = require("enzyme");
+var Adapter = require("enzyme-adapter-react-16");
+
+enzyme.configure({ adapter: new Adapter() });
+
+// import { Enzyme } from "enzyme";
+// import { Adapter } from "enzyme-adapter-react-16";
+// import {configure} from 'enzyme'
+
+// Enzyme.configure({ adapter: new Adapter() });
