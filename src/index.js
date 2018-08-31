493f038cbdada4af41defa001b1336803052d0de
wasswa-derick <derick.wasswa@andela.com>
2018-08-31 10:42:56 +0300

    remove unnecessary lines of code

--- src/index.js
+++ src/index.js
@@ -9,16 +9,8 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { applicationReducers } from "./reducers/combine-reducers";
-import JwtDecode from "jwt-decode";
-import { loginUser } from "./actions/authentication";
 
 const store = createStore(applicationReducers, applyMiddleware(thunk));
-
-const appAccessToken = localStorage.getItem("app-access-token");
-if (appAccessToken) {
-  const user = JwtDecode(appAccessToken);
-  store.dispatch(loginUser({ ...user, logInStatus: true }));
-}
 
 ReactDOM.render(
   <Provider store={store}>
